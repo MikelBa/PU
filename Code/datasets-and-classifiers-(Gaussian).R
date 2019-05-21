1862f6999978f5b92062fc36f5152ba0dcca5da0
MikelBa <mikelbarrene@gmail.com>
2019-05-21 11:15:50 +0200

    Cross validation experiment done with semi supervised learning

--- Code/datasets-and-classifiers-(Gaussian).R
+++ Code/datasets-and-classifiers-(Gaussian).R
@@ -1,11 +1,34 @@
+set.seed(2019)
+
+dataset_names <- apply(expand.grid(2^c(1:5),seq(2,0,-1),seq(400,1600,200)),1,function(spec){
+  paste("Variables",spec[1],"NormalMean",spec[2],"ProportionPos",spec[3]/2000, sep="_")
+})
+
+datasets <- lapply(as.list(data.frame(t(expand.grid(2^c(1:5),seq(2,0,-1),seq(400,1600,200))))), function(spec){
+  xpos <- matrix(rnorm(spec[1]*spec[3]), ncol = spec[1])
+  ypos <- rep(1, spec[3])
+  xneg <- matrix(rnorm(spec[1]*(2000-spec[3]),spec[2]), ncol = spec[1])
+  yneg <- rep(0, 2000-spec[3])
+  mydf <- data.frame(rbind(cbind(xpos,ypos),cbind(xneg,yneg)))
+  colnames(mydf)[colnames(mydf)=="ypos"] <- "y"
+  mydf[, 'y'] <- as.factor(mydf[, 'y'])
+  return(mydf)
+})
+
+names(datasets) <- dataset_names
 
 
+models <- sapply(dataset_names, function(variables) {
+  return(formula(y~.))
+})
+rm(dataset_names)
 
-y <- rep(c(1,2,3),20 ) #could use sample instead if you want this to be random as in docendo's answer
 
-#for the matrix of variables x
-#you need a matrix of 50 variables i.e. 50 columns and 60 rows i.e. 60x50 dimensions (=3000 table cells)
-x <- matrix( rnorm(3000), ncol=50 )
-
-#bind the 2 - y will be the first column 
-mymatrix <- cbind(y,x)
+classifiers<-list(
+  # "Supervised"=function(X,y,X_u,y_u) {LeastSquaresClassifier(X,y,intercept=TRUE,x_center=TRUE,scale=FALSE) },
+  # "Self-Learning"=function(X,y,X_u,y_u) {SelfLearning(X, y, X_u, method=LeastSquaresClassifier,intercept=TRUE,x_center=TRUE,scale=FALSE)},
+  # "USM"=function(X,y,X_u,y_u) {USMLeastSquaresClassifier(X,y,X_u,intercept=FALSE,x_center=TRUE,y_scale=TRUE) }, 
+  "ICLS"=function(X,y,X_u,y_u) {ICLeastSquaresClassifier(X,y,X_u,intercept=TRUE,x_center=TRUE,scale=FALSE) }
+  # "ICLS_prior"=function(X,y,X_u,y_u) { ICLeastSquaresClassifier(X,y,X_u,x_center=FALSE,scale=FALSE, lambda_prior=1000000, trueprob=mean(model.matrix(~y-1,data.frame(y=y_u))[,1])) },
+  # "Oracle"=function(X,y,X_u,y_u) {LeastSquaresClassifier(rbind(X,X_u),unlist(list(y,y_u)),intercept=TRUE,x_center=TRUE,scale=FALSE) }
+)
